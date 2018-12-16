39601f5ca8eb55a03e74c3fc8dd3177599d6d87a
ashwinarora <ashwinarora48@gmail.com>
2018-12-17 01:32:36 +0530

    Delegate Transfer

--- contracts/MajorCoin.sol
+++ contracts/MajorCoin.sol
@@ -13,7 +13,14 @@ contract MajorCoin{
     uint256 _value
   );
 
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public{
     balanceOf[msg.sender] = _initialSupply;
@@ -27,4 +34,22 @@ contract MajorCoin{
     emit Transfer(msg.sender, _to, _value);
     return true;
   }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);
+    require(allowance[_from][msg.sender] >= _value);
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_from][msg.sender] -= _value;
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+
 }
