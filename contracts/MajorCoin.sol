43b1af6d3c592fc9afc68ae1c63a1c6d7ea51583
ashwinarora <ashwinarora48@gmail.com>
2018-12-17 00:14:49 +0530

    Tranfer function

--- contracts/MajorCoin.sol
+++ contracts/MajorCoin.sol
@@ -2,9 +2,29 @@ pragma solidity ^0.4.2;
 
 contract MajorCoin{
 
-  uint256 public totalsupply;
+  string public name = "MajorCoin";
+  string public symbol = "SASA";
+  string public standard = "MajorCoin v1.0";
+  uint256 public totalSupply;
 
-  constructor() public{
-    totalsupply = 1000000;
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) public{
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var MajorCoin = artifacts.require("./MajorCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MajorCoin);
+  deployer.deploy(MajorCoin, 1000000);
 };
