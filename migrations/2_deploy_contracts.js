f4e1d0bf0800f4b5e633202e41160ac735112812
ashwinarora <ashwinarora48@gmail.com>
2018-12-17 05:09:11 +0530

    Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var MajorCoin = artifacts.require("./MajorCoin.sol");
+var MajorCoinSale = artifacts.require("./MajorCoinSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MajorCoin, 1000000);
+  deployer.deploy(MajorCoin, 1000000).then(function(){
+    var tokenPrice = 1000000000000000 // equals 0.001 ether
+    return deployer.deploy(MajorCoinSale, MajorCoin.address, tokenPrice)
+  });
 };
